da6d9beae35f734643d1821bc5a633d2da2d7855
mrbenboyy <mrbenboyy@gmail.com>
2025-04-20 15:18:36 +0100

    add authentication and role-checking middleware to user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const authMiddleware = require("../middlewares/authMiddleware");
+const checkRole = require("../middlewares/checkRole");
 
-router.get("/", userController.getUsers);
-router.post("/", userController.addUser);
+router.get("/", authMiddleware, checkRole(["admin"]), userController.getUsers);
+router.post("/", authMiddleware, checkRole(["admin"]), userController.addUser);
 
 module.exports = router;
